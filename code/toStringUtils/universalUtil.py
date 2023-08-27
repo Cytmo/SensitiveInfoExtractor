a24e5e4768520be89fc8bb5cce3f367ecfd18d03
Hua Zhangzhao <jeremyhua@foxmail.com>
2023-08-27 17:03:42 +0800

    feat: 解析.wps/.et, 提取文本

--- code/toStringUtils/universalUtil.py
+++ code/toStringUtils/universalUtil.py
@@ -8,6 +8,7 @@ universalUtil: 通用读取
 # 使用texttract通用提取文件中文本信息
 #  .csv, .doc, .docx, .eml, .epub, .gif, .htm, .html, .jpeg, .jpg, .json, .log, .mp3, .msg, .odt,
 # .ogg, .pdf, .png, .pptx, .ps, .psv, .rtf, .tab, .tff, .tif, .tiff, .tsv, .txt, .wav, .xls, .xlsx
+# doc文件需要 apt-get install antiword
 def universal_textract(file):
     text = textract.process(filename=file, encoding='utf-8')
     decoded_text = text.decode('utf-8')
