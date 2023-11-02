553ee698efaa4a52665e7c9cfb4cf5e42e7b6505
Hua Zhangzhao <jeremyhua@foxmail.com>
2023-11-02 10:41:29 +0800

    fix: 添加部分单向依赖

--- code/config/info_core_config.py
+++ code/config/info_core_config.py
@@ -1,32 +1,60 @@
 import yaml
 # This dictionary will store placeholders and their corresponding types
-PLACEHOLDERS_CORRESPONDING_TYPE = {}  
+PLACEHOLDERS_CORRESPONDING_TYPE = {}
 # 保存易混淆内容的位置 infomaion protection
 ITEM_PROTECTION_DICT = {}
 # 英文关键词列表
 ENG_KEYWORDS_LIST = ["-u", "-p", "IP", "port", "-h",
-                 "user", "password", "passw0rd", "address", "name", '\n']
+                     "user", "password", "passw0rd", "address", "name", '\n']
 # 英文替换词列表
 ENG_REPLACEMENT_DICT = {"-p": "password", "port": "port", "-u": "user", "user": "user", "password": "password",
-                    "-h": "address", "IP":"address","address": "address", "name": "name", "passw0rd": "password"}
+                        "-h": "address", "IP": "address", "address": "address", "name": "name", "passw0rd": "password"}
 # 中文关键字列表
 CHN_KEYWORDS_LIST = ["账号", "IP", "端口", "名称", "地址",
                      "姓名", "学号", "用户名", "密码", "密钥为", '\n']
 # 中文替换列表
 CHN_REPLACEMENT_DICT = {"账号": "user", "端口": "port", "名称": "user", "学号": "user", "用户名": "user",
-                        "密钥为": "password", "密码": "password", "IP":"address","地址": "address", "姓名": "name"}
+                        "密钥为": "password", "密码": "password", "IP": "address", "地址": "address", "姓名": "name"}
 # 信息提取列表
 INFO_PATTERN = {"user": "user", "password": "password",
-                "address": "address", "port": "port","phonenumber":"phonenumber","email":"email","ip":"address","url":"address"}
+                "address": "address", "port": "port", "phonenumber": "phonenumber", "email": "email", "ip": "address", "url": "address"}
 
+# TODO: 单项依赖, ID放后面
 # 单项依赖信息组---前者需要后者存在
-ONE_WAY_CONNECTED_INFO = {"port": "address",}
+ONE_WAY_CONNECTED_INFO = {"port": "address",
+                          "AWS MWS key": "AWS MWS ID",
+                          "AWS API Key": "AWS API Gateway",
+                          "AWS S3 Bucket": "AWS Access Key ID Value",
+                          "Github App Token": "GitHub",
+                          "Github OAuth Access Token": "GitHub",
+                          "Github Personal Access Token": "GitHub",
+                          "Google API Key": "Google (GCP) Service Account",
+                          "Google OAuth Access Token": "Google (GCP) Service Account",
+                          "SSH (DSA) private key": "SSH",
+                          "Slack Token": "Slack",
+                          "Slack User token": "Slack",
+                          "Slack User token": "Slack",
+                          "Slack access token": "Slackwebhook",
+                          "Square API Key": "Squareup",
+                          "Square OAuth Secret": "Squareup",
+                          "Square access token": "Squareup",
+                          "Stripe API Key": "Stripe",
+                          "Stripe Public Live Key": "Stripe",
+                          "Telegram Bot API Key": "Telegram Secret",
+                          "Twitter Access Token": "Twitter Secret Key",
+                          "Twitter OAuth": "Twitter Client ID",
+                          "awssecretkey": "awsaccesskeyid",
+                          "facebook_oauth": "facebook_access_token",
+                          "secretkey": "secretaccesskey",
+                          "twitteroauthaccesssecret": "twitteroauthaccesstoken",
+                          }
 
 # 双向依赖信息组---等价关系，前后都需要存在才成立
-TWO_WAY_CONNECTED_INFO = {"AWSsecretkey":"AWSaccesskey"}
+TWO_WAY_CONNECTED_INFO = {"AWSsecretkey": "AWSaccesskey"}
 
 # 信息提取替换词列表
-REPLACED_KEYWORDS_LIST = ["{user}", "{password}", "{address}", "{port}", "{phonenumber}", "{email}"]
+REPLACED_KEYWORDS_LIST = ["{user}", "{password}",
+                          "{address}", "{port}", "{phonenumber}", "{email}"]
 
 # 代码提取词列表
 SPECIAL_KEYWORDS_LIST = [
@@ -60,7 +88,3 @@ with open('config/rules-stable.yml', 'r') as yaml_file:
     for pattern in SENSITIVE_INFO_PATTERN['patterns']:
         name = pattern['pattern']['name'].strip()
         REPLACED_KEYWORDS_LIST.append("{"+name+"}")
-
-
-
-
